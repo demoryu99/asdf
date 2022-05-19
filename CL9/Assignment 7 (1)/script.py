1fe37878731736b83de42093d8b5894e74fb2b1b
demoryu <105877277+demoryu99@users.noreply.github.com>
2022-05-19 22:22:04 +0530

    Add files via upload

---
+++ CL9/Assignment 7 (1)/script.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask import jsonify, json
+import pandas as pd
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("Index.html")
+
+@app.route('/users')
+def disp_users():
+    data = pd.read_json('users.json')
+    data = data.values.tolist()
+    return jsonify(data)
+@app.route('/movies')
+def disp_movies():
+    data_movies = pd.read_json('movies.json')
+    data_movies = data_movies.values.tolist()
+    return jsonify(data_movies)
+@app.route('/booking')
+def disp_booking():
+    data_booking = pd.read_json('booking.json')
+    data_booking = data_booking.values.tolist()
+    return jsonify(data_booking)
+@app.route('/showtimes')
+def disp_showtimes():
+    data_showtimes = pd.read_json('showtimes.json')
+    data_showtimes = data_showtimes.values.tolist()
+    return jsonify(data_showtimes)
+if __name__ == '__main__':
+    app.run(debug=True) 
